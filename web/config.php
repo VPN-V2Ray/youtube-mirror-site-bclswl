4e9eb96b007ec27f9d28e402cb8032e40e7ecb79
wburke520 <41992290+wburke520@users.noreply.github.com>
2018-11-02 15:14:39 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -6,4 +6,27 @@ define('SITE_NAME', 'YouTube');
 define('TITLENAME', 'YouTube Mirror');
 define('EN2DEKEY', 'TXhEBihhDkmSpeHBBrrWahoPZUyS1jPsCwJGYgMoDClTpCTkbnJLQt7QHrhy5TjoFq4hvQ9VvkUoN1xoX1oyqIurmDdwwI2pJP');
 define('EMAIL', 'example@example.ne.jp');
+define('NAME', 'admin');
+define('PASSWORD', 'admin');
+define('LOGINHTML', '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="Login.css"/>
+</head>
+<body>
+<center>
+    <p id="login">
+    <h1>µÇÂ½</h1>
+    <form method="post" style="margin:0px auto;">
+        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <br>
+        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
+        <button class="but" type="submit">µÇÂ¼</button>
+    </form>
+    </p>
+</center>
+</body>
+</html>');
 ?>
