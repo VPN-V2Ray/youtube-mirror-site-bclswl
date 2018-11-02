0865b780f70f3c00913dd0a1c0351c8d665c099a
wburke520 <41992290+wburke520@users.noreply.github.com>
2018-11-02 15:30:22 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBy2VkQXTgCWkfWfgejVUcKH5GuO2DFAds');
-define('GJ_CODE', 'JP');
+define('GJ_CODE', 'US');
 define('SITE_NAME', 'YouTube');
-define('TITLENAME', 'YouTube Mirror');
+define('TITLENAME', '无忧YouTube');
 define('EN2DEKEY', 'TXhEBihhDkmSpeHBBrrWahoPZUyS1jPsCwJGYgMoDClTpCTkbnJLQt7QHrhy5TjoFq4hvQ9VvkUoN1xoX1oyqIurmDdwwI2pJP');
 define('EMAIL', 'example@example.ne.jp');
 define('NAME', 'admin');
